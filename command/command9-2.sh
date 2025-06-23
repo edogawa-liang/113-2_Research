ea334ba33d0940f331eccfb72b1dcd8633e90f64
edogawa-liang <tiffany217.h@gmail.com>
2025-06-23 18:49:19 +0800

    revise

--- command/command9-2.sh
+++ command/command9-2.sh
@@ -6,7 +6,7 @@ set -x
 # ------------------------------------------------------------
 ## Feature to Node (含節點邊 & 特徵邊)
 # python stage2_expsubg.py --dataset Cora --explainer_type GNNExplainer --run_mode stage2_feature2node_nn_fn --stage1_path saved/stage1_feature2node_nn_fn --feature_to_node --structure_mode random+imp 
-python stage2_expsubg.py --dataset CiteSeer --explainer_type GNNExplainer --run_mode stage2_feature2node_nn_fn --stage1_path saved/stage1_feature2node_nn_fn --feature_to_node --structure_mode random+imp 
+python stage2_expsubg.py --dataset CiteSeer --explainer_type GNNExplainer --run_mode stage2_feature2node_nn_fn --stage1_path saved/stage1_feature2node_nn_fn --feature_to_node --structure_mode random+imp --check_unexplained
 python stage2_expsubg.py --dataset Amazon --explainer_type GNNExplainer --run_mode stage2_feature2node_nn_fn --stage1_path saved/stage1_feature2node_nn_fn --feature_to_node --structure_mode random+imp 
 
 ## Feature to Node (只有特徵邊)
