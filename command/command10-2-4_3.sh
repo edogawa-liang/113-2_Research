a0dfdd2642cc7e7081352ad5f9aa5636f4419869
edogawa-liang <tiffany217.h@gmail.com>
2025-07-06 21:52:02 +0800

    move table code

---
+++ command/command10-2-4_3.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -x 
+
+# ==================== Stage 3 移除解釋子圖後========================
+
+# CFExplainer (補算)
+
+# # 1. 移除結構 (節點邊)
+# ### [2] explainer select
+# python train_remaining_main.py --dataset Cora --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --fraction 0.1 --base_dir saved/stage2 --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_edge --note split0_cf_edge --stage1_path saved/stage1
+# python train_remaining_main.py --dataset CiteSeer --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --fraction 0.1 --base_dir saved/stage2 --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_edge --note split0_cf_edge --stage1_path saved/stage1
+# python train_remaining_main.py --dataset Amazon --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --fraction 0.1 --base_dir saved/stage2 --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_edge --note split0_cf_edge --stage1_path saved/stage1
+# python train_remaining_main.py --dataset FacebookPagePage --normalize  --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --fraction 0.1 --base_dir saved/stage2 --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_edge --note split0_cf_edge --stage1_path saved/stage1
+# python train_remaining_main.py --dataset GitHub --normalize --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --fraction 0.1 --base_dir saved/stage2 --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_edge --note split0_cf_edge --stage1_path saved/stage1
+
+
+# # 3. 移除特徵 (特徵不同)
+# ## (2) feature to Node (含節點邊 & 特徵邊)
+# ### [2] explainer select
+# python train_remaining_main.py --dataset Cora --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_nn_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_nn_fn --fraction_feat 0.1  --feature_to_node --stage1_path saved/stage1_feature2node_nn_fn
+# python train_remaining_main.py --dataset CiteSeer --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_nn_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_nn_fn --fraction_feat 0.1  --feature_to_node --stage1_path saved/stage1_feature2node_nn_fn
+# # python train_remaining_main.py --dataset Amazon --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_nn_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_nn_fn --fraction_feat 0.1  --feature_to_node --stage1_path saved/stage1_feature2node_nn_fn
+# python train_remaining_main.py --dataset FacebookPagePage --normalize --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_nn_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_nn_fn --fraction_feat 0.1  --feature_to_node --stage1_path saved/stage1_feature2node_nn_fn
+# # python train_remaining_main.py --dataset GitHub --normalize --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_nn_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_nn_fn --fraction_feat 0.1  --feature_to_node --stage1_path saved/stage1_feature2node_nn_fn
+
+
+## (3) feature to Node (只有特徵邊)
+### [2] explainer select
+python train_remaining_main.py --dataset Cora --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_fn --fraction_feat 0.1 --feature_to_node --only_feature_node --stage1_path saved/stage1_feature2node_fn
+python train_remaining_main.py --dataset CiteSeer --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_fn --fraction_feat 0.1 --feature_to_node --only_feature_node --stage1_path saved/stage1_feature2node_fn
+python train_remaining_main.py --dataset Amazon --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_fn --fraction_feat 0.1 --feature_to_node --only_feature_node --stage1_path saved/stage1_feature2node_fn
+python train_remaining_main.py --dataset FacebookPagePage --normalize --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_fn --fraction_feat 0.1 --feature_to_node --only_feature_node --stage1_path saved/stage1_feature2node_fn
+# python train_remaining_main.py --dataset GitHub --normalize --model GCN2 --epochs 1 --lr 0.01 --selector_type explainer --base_dir saved/stage2_feature2node_fn --explainer_name CFExplainer --node_choose all_train --run_mode try_stage3_CFExplainer_difffeat --note split0_feat2node_difffeat_fn --fraction_feat 0.1 --feature_to_node --only_feature_node --stage1_path saved/stage1_feature2node_fn
+
+
+
+wait
